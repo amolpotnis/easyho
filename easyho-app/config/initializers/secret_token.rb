7de4131882459c6f08214fcbbf640a37d04852fc
amolpotnis <amolpotnis@server>
2012-10-13 11:24:20 +0530

    - BAsic dumb app

---
+++ easyho-app/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+EasyhoApp::Application.config.secret_token = 'a6fa6c70f0f018538d6e1f68239189c2c0001c7102d99ebec026dd6ffd9924fcbfc61f4fabefbb91adcf51b0b03ea6ac7d668cfc2de3346e472af73c627dc2c3'
